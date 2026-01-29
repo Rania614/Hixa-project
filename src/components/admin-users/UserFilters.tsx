3ae4cb3048cac47be4eb630561d686a2e38f9409
Rania614 <Raniaabdelnasser93@gmail.com>
2026-01-29 00:32:36 -0800

    edit filter in admin dashboard

--- src/components/admin-users/UserFilters.tsx
+++ src/components/admin-users/UserFilters.tsx
@@ -134,7 +134,7 @@ export const UserFilters = ({
             {/* Category Filter */}
             <div className="w-full md:w-64">
                 <Popover>
-                    <PopoverTrigger asChild>
+                    {/* <PopoverTrigger asChild>
                         <Button
                             variant="outline"
                             role="combobox"
@@ -155,7 +155,7 @@ export const UserFilters = ({
                             </div>
                             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                         </Button>
-                    </PopoverTrigger>
+                    </PopoverTrigger> */}
                     <PopoverContent className="w-64 p-0">
                         <Command>
                             <CommandInput placeholder={language === 'en' ? "Search category..." : "البحث عن تخصص..."} />
